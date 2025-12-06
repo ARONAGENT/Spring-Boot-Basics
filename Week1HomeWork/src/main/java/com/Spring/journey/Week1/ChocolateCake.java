5e60e03354fcf2dd3f831226c65ce277b3746b44
Rohan Uke <ukerohan7@gmail.com>
2025-12-07 02:30:06 +0530

    Done With Home Week 1 Homework

--- Week1HomeWork/src/main/java/com/Spring/journey/Week1/ChocolateCake.java
+++ Week1HomeWork/src/main/java/com/Spring/journey/Week1/ChocolateCake.java
@@ -9,18 +9,22 @@ import org.springframework.stereotype.Component;
 @Component
 @ConditionalOnProperty(name="bake.cake",havingValue = "Chocolate")
 public class ChocolateCake implements Frosting,Syrup{
-    public String getFrosting(){
-        return "Chocolate Cake get Frosting ...";
+    public String getFrosting() {
+        return "Chocolate Cake gets Frosting ...";
     }
-    public String getSyrup(){
-        return "Chocolate Cake get Syrup ...";
+
+    public String getSyrup() {
+        return "Chocolate Cake gets Syrup ...";
     }
+
     @PostConstruct
-    void beforeBake(){
-        System.out.println("baker makes chocolate cake ...");
+    void beforeBake() {
+        System.out.println("Baker prepares the chocolate cake ...");
     }
+
     @PreDestroy
-    void clean(){
-        System.out.println("Chocolate Cake bakes .... Successfully");
+    void clean() {
+        System.out.println("Chocolate Cake baked successfully!");
     }
+
 }
